3ac82cf4444130329057fe96bf4104e372f9b51b
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-03-02 17:57:32 +0300

    Homepage

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Samples</a>
+                            <a className="nav-link active" aria-current="page" href="#">Articles</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" href="#">Sign Up</a>
