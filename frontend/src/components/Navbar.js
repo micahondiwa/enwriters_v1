0cc2015b515bf451c1acf70d240b3d6d7f55f1e0
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-03-02 17:46:58 +0300

    Nav bar items

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,20 +4,23 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Articles</a>
+                <a className="navbar-brand" href="#">Home</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="#">Samples</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Sign Up</a>
+                            <a className="nav-link active" href="#">Sign Up</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Login</a>
+                            <a className="nav-link active" href="#">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Get Started</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" aria-disabled="true">Contact</a>
