6f51657b342f399da942f17cc48212991c738da7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-03-02 17:26:10 +0300

    Forked from v_0

---
+++ config.py
@@ -0,0 +1,23 @@
+"""Contains all configurations for the app"""
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+class DevConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_ECHO=True
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'test.db')
+    SQLALCHEMY_ECHO=False
+    TESTING=True
